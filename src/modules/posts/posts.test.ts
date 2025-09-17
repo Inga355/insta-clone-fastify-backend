0f7213ac293dfcaa48f3f65c1bc8e1f59e48c0d6
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-09-17 21:28:40 +0200

    test(posts): fix failing test by mocking service.create return value

--- src/modules/posts/posts.test.ts
+++ src/modules/posts/posts.test.ts
@@ -1,37 +1,37 @@
-import Fastify from "fastify";
-import { postsRoutes } from "./posts.routes";
+import Fastify from "fastify"
+import { postsRoutes } from "./posts.routes"
 
 describe("POST /posts", () => {
-  it("should create a new post and return it with a 201 status code", async () => {
-    const app = Fastify();
+    it("should create a new post and return it with a 201 status code", async () => {
+        const app = Fastify()
 
-    const newPostPayload = {
-      img_url: "http://example.com/new-image.jpg",
-      caption: "A brand new post from our test!",
-    };
+        const newPostPayload = {
+            img_url: "http://example.com/new-image.jpg",
+            caption: "A brand new post from our test!",
+        }
 
-    const createdPost = { ...newPostPayload, id: 1 };
+        const createdPost = { ...newPostPayload, id: 1 }
 
-    app.decorate("transactions", {
-      posts: {
-        create: jest.fn(),
-        getAll: jest.fn(),
-        getById: jest.fn(),
-      },
-      reels: {
-        getAll: jest.fn(),
-      },
-    });
+        app.decorate("transactions", {
+            posts: {
+                create: jest.fn().mockReturnValue(createdPost),
+                getAll: jest.fn(),
+                getById: jest.fn(),
+            },
+            reels: {
+                getAll: jest.fn(),
+            },
+        })
 
-    app.register(postsRoutes);
+        app.register(postsRoutes)
 
-    const response = await app.inject({
-      method: "POST",
-      url: "/posts",
-      payload: newPostPayload,
-    });
+        const response = await app.inject({
+            method: "POST",
+            url: "/posts",
+            payload: newPostPayload,
+        })
 
-    expect(response.statusCode).toBe(201);
-    expect(JSON.parse(response.payload)).toEqual(createdPost);
-  });
-});
+        expect(response.statusCode).toBe(201)
+        expect(JSON.parse(response.payload)).toEqual(createdPost)
+    })
+})
